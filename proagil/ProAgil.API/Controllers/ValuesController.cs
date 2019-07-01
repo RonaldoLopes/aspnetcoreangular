b1d8aa07c8aa4db2c239037f662bf19554be0b8a
Ronaldo Cassiano Lopes Alves <ronaldorclamaster@gmail.com>
2019-07-01 09:58:56 -0300

    iniciando com banco

--- proagil/ProAgil.API/Controllers/ValuesController.cs
+++ proagil/ProAgil.API/Controllers/ValuesController.cs
@@ -3,25 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProAgil.API.Data;
+using ProAgil.API.Model;
 
 namespace ProAgil.API.Controllers
 {
-    [Route("site/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        public readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Evento>> Get()
         {
-            return new string[] { "value1", "value2", "value3" };
+            return _context.Eventos.ToList();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Evento> Get(int id)
         {
-            return "value";
+            return new Evento[] { 
+                new Evento(){
+                    EnventoId = 1,
+                    Tema = "Angular e .net core",
+                    Local = "Belo Horizonte",
+                    Lote = "1 Lote",
+                    QtdePessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                },
+                new Evento(){
+                    EnventoId = 2,
+                    Tema = "Angular e suas novidade",
+                    Local = "São Paulo",
+                    Lote = "2 Lote",
+                    QtdePessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+                }
+            }.FirstOrDefault(x => x.EnventoId == id);
         }
 
         // POST api/values
---
+++ proagil/ProAgil.API/Migrations/20190701124323_imgUrl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.API.Migrations
+{
+    public partial class imgUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "imagemURL",
+                table: "Eventos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "imagemURL",
+                table: "Eventos");
+        }
+    }
+}
