1b5a273dddcec30650095369c0b43d98046ec496
Ronaldo Cassiano Lopes Alves <ronaldorclamaster@gmail.com>
2019-07-02 08:57:49 -0300

    .

--- proagil/ProAgil.API/Controllers/ValuesController.cs
+++ proagil/ProAgil.API/Controllers/ValuesController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProAgil.API.Data;
 using ProAgil.API.Model;
 
@@ -19,33 +21,33 @@ namespace ProAgil.API.Controllers
         }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<Evento>> Get()
+        public async Task<IActionResult> Get()
         {
-            return _context.Eventos.ToList();
+            try
+            {
+                var results = await _context.Eventos.ToListAsync();
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+            
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<Evento> Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
-            return new Evento[] { 
-                new Evento(){
-                    EnventoId = 1,
-                    Tema = "Angular e .net core",
-                    Local = "Belo Horizonte",
-                    Lote = "1 Lote",
-                    QtdePessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento(){
-                    EnventoId = 2,
-                    Tema = "Angular e suas novidade",
-                    Local = "São Paulo",
-                    Lote = "2 Lote",
-                    QtdePessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-                }
-            }.FirstOrDefault(x => x.EnventoId == id);
+           try
+            {
+                var results = await _context.Eventos.FirstOrDefaultAsync(x => x.EnventoId == id);
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
         }
 
         // POST api/values
