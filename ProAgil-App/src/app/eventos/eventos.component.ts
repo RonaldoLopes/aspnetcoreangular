e99983c0a40c2d759b3057c77952a487ba914975
Ronaldo Cassiano Lopes Alves <ronaldorclamaster@gmail.com>
2019-07-04 09:18:24 -0300

    Organizando projetos

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,12 +8,38 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventosComponent implements OnInit {
 
+  _filtroLista: string;
+  get filtroLista():string{
+    return this._filtroLista;
+  }
+  set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEvento(this.filtroLista) : this.eventos;
+  }
+
+  eventosFiltrados: any = [];
   eventos: any = [];
+  imagemLargura: number = 50;
+  imagemMargem: number = 2;
+  mostrarImagem = false;
+  
 
    constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
+  }
+
+
+  filtrarEvento(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !==  -1
+    );
+  }
+
+  alternarImagem(){
+    this.mostrarImagem = !this.mostrarImagem;
   }
 
   getEventos(){
